2d74ae72c5fc28b0fdedb05906bc82af2677dc12
Jack Shunn <jack.shunn@gmail.com>
2022-07-27 16:30:08 -0600

    Test new functions api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './styles/App.css';
 import Navbar from './components/Navbar';
 import Main from './components/Main';
+import {useState, useEffect} from 'react'
 
 
 export default function App() {
-  return (
-    <div className='h-screen flex flex-col'>
-      <Navbar/>
-      <Main />
-    </div>
-  );
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const { text } = await( await fetch(`/api/data`)).json();
+      setData(text);
+    })();
+  });
+
+  return <div>{data}</div>;
+  // return (
+  //   <div className='h-screen flex flex-col'>
+  //     <Navbar/>
+  //     <Main />
+  //   </div>
+  // );
 }
