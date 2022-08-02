2b9f83ec7a0d7a502ffb32a8327bb8e28ae6bef6
Jack Shunn <jack.shunn@gmail.com>
2022-08-02 16:31:55 -0600

    eslint fix

--- src/components/FocusedList.js
+++ src/components/FocusedList.js
@@ -3,11 +3,7 @@ import React from "react";
 export default function FocusedList(props) {
 
     function handleChange(event){
-        setList(prev => {
-            let newList = {...prev};
-            newList.items[0].text = event.target.value;
-            return newList;
-        })
+        props.handleChangedData({title:"Title1",items:[{text:event.target.value}]})
     }
 
     function handleInsideClick(event){
@@ -19,7 +15,7 @@ export default function FocusedList(props) {
         <div className="flex">
             <span className="text-white text-xl my-auto ml-5">1.</span>
             <div className="flex-1 m-5 rounded-xl h-11 bg-customColor-darkBlue flex">
-                <input type="text" className="flex-1 bg-transparent text-white text-xl mx-3" value={list.items[0].text} onChange={handleChange}></input>
+                <input type="text" className="flex-1 bg-transparent text-white text-xl mx-3" value={props.list.items[0].text} onChange={handleChange}></input>
                 <span className="h-11 w-1 bg-customColor-orange"/>
                 <input type="date" className="bg-transparent text-white text-xl mx-3"></input>
             </div>
